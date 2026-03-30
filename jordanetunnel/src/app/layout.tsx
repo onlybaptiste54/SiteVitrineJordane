fe0283e5bac71c69f135efaa03be6f05331e7f7f
OnlyBaptiste <157699823+OnlyBaptiste@users.noreply.github.com>
2026-03-30 10:03:56 +0200

    ajout ads

--- jordanetunnel/src/app/layout.tsx
+++ jordanetunnel/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const rubik = Rubik({
@@ -23,6 +24,27 @@ export default function RootLayout({
       <body
         className={`${rubik.variable} font-sans antialiased bg-merino-ecru text-merino-black min-h-screen`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M87CVCF3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
+        {/* Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M87CVCF3');
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
